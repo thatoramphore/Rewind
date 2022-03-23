0850e51233e077ce9cb1afd2e201bd2e2f36fb04
thatoramphore <r2.thato@gmail.com>
2022-03-23 21:58:47 +0200

    Add functionality to the app
    Added functionality using JavaScript to the app and updated index.html, code.html and site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello, World!");
+//Get the string from the user input
+//CONTROLLER
+function getValue(){
+    //make alert invisible afrom the beginning
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    //pass userString to reverseString
+    let reverse = reverseString(userString);
+
+    //display reversed string
+    displayString(reverse);
+}
+
+//Reverse the string
+//LOGIC
+function reverseString(userString){
+    let newString = [];
+    for(let i=userString.length-1; i>=0; i--){
+        newString += userString[i];
+    }
+    return newString;
+}
+
+//Display reversed string to the user
+//VIEW
+function displayString(reverse){
+    //write message to the screen
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${reverse}`;
+    
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+    document.getElementById("alert").classList.add("visible");
 }
